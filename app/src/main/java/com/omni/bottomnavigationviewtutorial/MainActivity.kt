33ca6a63168886cbecb865dc6a25cb3638811e1e
Omni <engomneya.osman@gmail.com>
2018-11-24 15:16:30 +0200

    Initial commit

---
+++ app/src/main/java/com/omni/bottomnavigationviewtutorial/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.omni.bottomnavigationviewtutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottom_nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
