0b2ebe085c8deb39351ee4c449a392c95681e079
Omni <engomneya.osman@gmail.com>
2019-01-03 22:52:32 +0200

    update appcompat library to -beta01 to fix crash when press back

--- app/src/main/java/com/omni/bottomnavigationviewtutorial/MainActivity.kt
+++ app/src/main/java/com/omni/bottomnavigationviewtutorial/MainActivity.kt
@@ -1,33 +1,51 @@
 package com.omni.bottomnavigationviewtutorial
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        //region set up navigation view
+        navController = this.findNavController(R.id.my_nav_host_fragment)
+        setUpActionbarNavigation()
+        //setupBottomNavMenu
+        bottom_nav_view.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+//        NavigationUI.setupWithNavController(bottom_nav_view , navController)
+
+        //endregion
+
+        navController.addOnNavigatedListener { _, destination ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Integer.toString(destination.id)
+            }
+
+            Log.d("NavigationActivity", "Navigated to $dest")
+        }
+
+
     }
+
+
+
+    private fun setUpActionbarNavigation() {
+//        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
 }
---
+++ app/src/main/java/com/omni/bottomnavigationviewtutorial/NotificationsFragment.kt
@@ -0,0 +1,30 @@
+package com.omni.bottomnavigationviewtutorial
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class NotificationsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment
+         * @return A new instance of fragment HomeFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = NotificationsFragment()
+    }
+}
